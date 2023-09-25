2388e35bc6f73087cce6d4fe7565bcb807f5248e
Antisessa <118179929+Antisessa@users.noreply.github.com>
2023-09-25 17:32:58 +0300

    start point

---
+++ src/main/java/ru/antisessa/CarRefuels/AnalyzingCarRefuelsApplication.java
@@ -0,0 +1,13 @@
+package ru.antisessa.CarRefuels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzingCarRefuelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzingCarRefuelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/antisessa/CarRefuels/AnalyzingCarRefuelsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.antisessa.CarRefuels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzingCarRefuelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
